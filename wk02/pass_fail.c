07c840313a87b9c0f66f57a743b29c4306d42714
helenaKertesz <helena.kertesz1@gmail.com>
2019-06-12 09:02:08 +1000

    wk02 skeleton

---
+++ wk02/pass_fail.c
@@ -0,0 +1,11 @@
+// Author: Wed09Sitar Tut
+// Date created: 11-06-2019
+// Reads in a mark and then prints pass or fail
+
+
+int main(void){
+
+    // TODO
+
+    return 0;
+}
---
+++ wk02/rectangle.c
@@ -0,0 +1,12 @@
+// Author: Wed09Sitar Tut
+// Date created: 11-06-2019
+// Given two edges, print the area of the rectangle
+
+#include<stdio.h>
+
+int main(void){
+
+    // TODO
+
+    return 0;
+}

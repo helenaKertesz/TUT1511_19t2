df51d93f304b77eeb0f2ec0c0d332a1746c51ac1
helenaKertesz <helena.kertesz1@gmail.com>
2019-06-13 08:45:12 +1000

    wk03 skeleton page and code

---
+++ wk03/cm2feet.c
@@ -0,0 +1,17 @@
+// Author: wed09-sitar and wed13-kora
+// Date created: 19/06/2019
+// Converts cm to feet
+
+#include <stdio.h>
+
+int main(void) {
+
+    int heightCm, heightFeet;
+
+    printf("Please enter your height in centimeters: ");
+    scanf("%d", &heightCm);
+
+    // TODO
+    
+    return 0;
+}
---
+++ wk03/count42.c
@@ -0,0 +1,12 @@
+// Author: wed09-sitar and wed13-kora
+// Date created: 19/06/2019
+// Prints out 1 to 42
+
+#include <stdio.h>
+
+int main(void) {
+
+    // TODO
+    
+    return 0;
+}

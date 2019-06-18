6c72931965a4281810fba9da08c3c1cdd40b5318
helenaKertesz <helena.kertesz1@gmail.com>
2019-06-18 20:59:06 +1000

    clean up wk01

---
+++ wk01/face.c
@@ -0,0 +1,15 @@
+// Author: Wed09-sitar and Wed13-kora
+// Date created: 05/06/2019
+// Prints out a face
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf( "~ ~\n");
+    printf( "0 0\n");
+    printf( " o \n");
+    printf("\\_/\n");
+    
+    return 0;
+}
---
+++ wk01/hello.c
@@ -0,0 +1,12 @@
+// Author: Andrew Taylor (andrewt@unsw.edu.au)
+// Date created: January 2018
+// A very simple C program
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf( "Hello world!\n");
+    
+    return 0;
+}

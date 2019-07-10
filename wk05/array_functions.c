b7dd304072b7acfc0dc210bba2fb82fc7fc06455
helenaKertesz <helena.kertesz1@gmail.com>
2019-07-10 17:01:31 +1000

    wk05 put up

--- wk05/array_functions.c
+++ wk05/array_functions.c
@@ -13,23 +13,49 @@ double average(int n, int array[n]);
 int main(){
 
     int a1[5] = {1, 2, 3, 4, 5};
-    int a2[5] = {5, 4, 3, 2, 1};
 
+    print_array(5, a1);
+    printf("Average is %lf\n", average(5, a1));
+
+    int a2[6];
+    int num_read = scanf_array(6, a2);
+    printf("Read in %d values\n", num_read);
+    print_array(num_read, a2);
+    printf("Average is %lf\n", average(num_read, a2));
 
     return 0;
 }
 
 void print_array(int n, int array[n]){
-    // TODO
+    printf("[ ");
+
+    int i = 0;
+    while( i < n ){
+        printf("%d ", array[i]);
+        i++;
+    }
+    
+    printf("]\n");
 }
 
 int scanf_array(int n, int array[n]){
     
     printf("Please enter %d integers: ", n );
-
-    // TODO
+    int i = 0;
+    while( i < n && scanf("%d", &array[i]) == 1 ){
+        i++;
+    }
+    return i;
 }
 
 double average(int n, int array[n]){
-    // TODO
+    int i = 0;
+    double average = 0;
+    while( i < n ){
+        average = average + array[i];
+        i++;
+    }
+
+    average = average / n;
+    return average;
 }

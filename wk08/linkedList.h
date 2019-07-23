20c63c81b85b352e27917be7de3ca04391d98e81
helenaKertesz <helena.kertesz1@gmail.com>
2019-07-23 16:45:51 +1000

    linked lists in two files

--- wk08/linkedList.h
+++ wk08/linkedList.h
@@ -1,2 +1,15 @@
+struct overall {
+    struct node *start;
+    struct node *end;
+};
 
+struct node {
+    int data;
+    struct node *next;
+};
 
+struct node *new_node(int data);
+void insert_at_front(struct overall *o, int data);
+void insert_before_end(struct overall *o, int data);
+struct overall* new_list();
+void print_list (struct overall *o);

0db2257b3d9bd36eb743673f1f36940bfdae2b2e
z5059868 <z5059868@cse.unsw.edu.au>
2019-07-23 16:25:53 +1000

    wk07

--- wk07/pets.c
+++ wk07/pets.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 
+struct pet {
+	int weight;
+	int age;
+	char name[100];
+	char type [100];
+}
+
 void age_fluffy( struct pet * my_pet );
 
 int main(){
+
+	struct pet my_pet;
+	
+	my_pet.age = 7;
+	age_fluffy( &my_pet );
 }
 
 void age_fluffy( struct pet * my_pet ){
+	
+	my_pet->age = 13;
+	//(*my_pet).age = 13;
 }
+
+
+
+
+
+
+
+
+
+
